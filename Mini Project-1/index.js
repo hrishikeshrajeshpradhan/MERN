f64e890c5a6960cfdd31f6c90712b624a6fd1312
Hrishikesh Pradhan <hrishikesh.pradhan@siemens-healthineers.com>
2024-07-06 18:33:42 +0530

    Basic html and switch tab function

--- Mini Project-1/index.js
+++ Mini Project-1/index.js
@@ -1,32 +1,40 @@
-async function fetchWeatherDetails(){
-    try{
-        let city="goa";
-        let API_key="6385bdf609957a0480a4c6788e5dd214";
-        let url="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+API_key;
-        const response= await fetch(url);
-        const data=await response.json();
-        
-        renderWeatherDetails(data);
-        console.log(data);
+const userTab=document.querySelector("[data-userWeather]");
+const searchTab=document.querySelector("[data-searchWeather]")
+const userContainer=document.querySelector(".main-conatiner")
 
-    }catch(err){
-        console.log("can not fetch data");
+const grantAccessContainer=document.querySelector(".grant-location-container")
+
+const searchForm=document.querySelector("[data-searchForm]")
+const loadingScreen=document.querySelector(".loading-container")
+const userInfo=document.querySelector(".user-info-container")
+
+let currentTab=userTab;
+const API_KEY="6385bdf609957a0480a4c6788e5dd214";
+currentTab.classList.add("current-tab")
+
+function switchTab(clickedtab){
+    if(clickedtab!=currentTab){
+        currentTab.classList.remove("current-tab");
+        currentTab=clickedtab;
+        currentTab.classList.add("current-tab")
+
+        if(searchForm.classList.contains("active")){
+            //search tab par abhi hai, userTab par jana hai
+            searchForm.classList.remove("active");
+            userInfo.classList.remove("active");
+            loadingScreen.classList.add("active");
+        }else{
+            loadingScreen.classList.remove("active");
+            userInfo.classList.remove("active");
+            searchForm.classList.add("active");
+            getFromSessionStorage();
+        }
     }
+
 }
-function renderWeatherDetails(data){
-        let newPara=document.createElement('h2');
-        newPara.textContent=data["main"]["temp"]+ " K";
-        document.body.appendChild(newPara);
-}
-function getCurrentPosition(){
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(showPosition);
-    }else{
-        console.log("Geolocation is not supported");
-    }
-}
-function showPosition(position){
-    let lat=position.coords.latitude;
-    let long=position.coords.longitude;
-    console.log(lat, long);
-}
+userTab.addEventListener("click",()=>{
+    switchTab(userTab);
+})
+searchTab.addEventListener("click",()=>{
+    switchTab(searchTab);
+})
